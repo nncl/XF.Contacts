7c3805ac9fd15e56773e63fdc904182d9b284d6d
= <nncl@live.com>
2017-05-23 20:12:14 -0300

    Initial check-in of module XF.Contatos

---
+++ XF.Contatos/XF.Contatos/Model/Contato.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XF.Contatos.ViewModel;
+
+namespace XF.Contatos.Model
+{
+    public interface IContato
+    {
+        void GetMobileContacts(ContatoViewModel vm);
+    }   
+
+    public class Contato
+    {
+        public Contato()
+        {
+            Telefones = new List<Telefone>();
+        }
+
+        public string Nome { get; set; }
+        public string SobreNome { get; set; }
+        public string DisplayName { get; set; }
+        public List<Telefone> Telefones { get; set; }
+        public string Numero
+        {
+            get
+            {
+                if (Telefones != null)
+                    return Telefones.FirstOrDefault().Numero;
+
+                return string.Empty;
+            }
+        }
+    }
+
+    public class Telefone
+    {
+        public Telefone() { }
+
+        public PhoneType Tipo { get; set; }
+        public string Descricao { get; set; }
+        public string Numero { get; set; }
+    }
+
+    public enum PhoneType
+    {
+        Home = 0,
+        HomeFax = 1,
+        Work = 2,
+        WorkFax = 3,
+        Pager = 4,
+        Mobile = 5,
+        Other = 6
+    }
+}
