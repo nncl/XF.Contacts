cce5d85f2161cb4feef0d24ddc914661ee9c6c08
= <nncl@live.com>
2017-05-27 13:51:03 -0300

    Camera functionality

--- XF.Contatos/XF.Contatos.Android/MainActivity.cs
+++ XF.Contatos/XF.Contatos.Android/MainActivity.cs
@@ -1,16 +1,21 @@
 ﻿using System;
+using XF.Contatos.API;
+using Xamarin.Forms;
+using Xamarin.Media;
 
 using Android.App;
+using Android.Content;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
 
+[assembly: Dependency(typeof(XF.Contatos.Droid.MainActivity))]
 namespace XF.Contatos.Droid
 {
     [Activity(Label = "XF.Contatos", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity, ICamera
     {
         protected override void OnCreate(Bundle bundle)
         {
@@ -22,6 +27,39 @@ namespace XF.Contatos.Droid
             global::Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new App());
         }
+
+		public void CapturarFoto()
+		{
+			var context = Forms.Context as Activity;
+			var captura = new MediaPicker(context);
+
+			var intent = captura.GetTakePhotoUI(new StoreCameraMediaOptions
+			{
+				DefaultCamera = CameraDevice.Rear,
+				Name = string.Format("foto_{0}.jpg", DateTime.Now.ToString()),
+				Directory = "Fiap"
+			});
+			context.StartActivityForResult(intent, 1);
+		}
+
+		public void SelecionarFoto()
+		{
+			var context = Forms.Context as Activity;
+			var captura = new MediaPicker(context);
+
+			var intent = captura.GetPickPhotoUI();
+            context.StartActivityForResult(intent, 1);
+		}
+
+		protected override async void OnActivityResult(int requestCode, Result resultCode, Intent data)
+		{
+			if (resultCode == Result.Canceled) return;
+
+			var mediaPath = await data.GetMediaFileExtraAsync(Forms.Context);
+			System.Diagnostics.Debug.WriteLine(mediaPath.Path);
+
+			MessagingCenter.Send<ICamera, string>(this, "cameraFoto", mediaPath.Path);
+		}
     }
 }
 
--- XF.Contatos/XF.Contatos/View/DetailsView.xaml.cs
+++ XF.Contatos/XF.Contatos/View/DetailsView.xaml.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 using XF.Contatos.ViewModel;
+using XF.Contatos.API;
 
 namespace XF.Contatos.View
 {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class DetailsView : ContentPage
     {
         DetailViewModel vmDetail;
@@ -13,10 +19,34 @@ namespace XF.Contatos.View
         public DetailsView(XF.Contatos.Model.Contato contato)
         {
             var detail = DetailViewModel.GetDetail(contato);
-             vmDetail = new DetailViewModel(detail);
+            vmDetail = new DetailViewModel(detail);
             BindingContext = vmDetail;
 
             InitializeComponent();
         }
+
+		private void btnCamera_Clicked(object sender, EventArgs e)
+		{
+			ICamera capturar = DependencyService.Get<ICamera>();
+			capturar.CapturarFoto();
+
+			MessagingCenter.Subscribe<ICamera, string>(this, "cameraFoto",
+				(objeto, image) =>
+				{
+					this.imgFoto.Source = ImageSource.FromFile(image);
+				});
+		}
+
+		private void btnSelecionar_Clicked(object sender, EventArgs e)
+		{
+			ICamera capturar = DependencyService.Get<ICamera>();
+			capturar.SelecionarFoto();
+
+			MessagingCenter.Subscribe<ICamera, string>(this, "cameraFoto",
+				(objeto, image) =>
+				{
+					this.imgFoto.Source = ImageSource.FromFile(image);
+				});
+		}
     }
 }
